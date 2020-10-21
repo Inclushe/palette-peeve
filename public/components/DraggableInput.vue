79179b11868bee1ff785318b73792bf454d27e8a
Ethan <ethan@inclushe>
2020-10-21 17:05:50 -0400

    Revert "Revert "Added component for sidebar section""
    This reverts commit 58c28341d1db2a8993f8a1f03f191dc6da68bde2.

--- public/components/DraggableInput.vue
+++ public/components/DraggableInput.vue
@@ -65,10 +65,15 @@ export default {
       },
       set: function (newValue) {
         let adjustedValue
+        console.log(this.selected)
         if (this.type === 'hue') {
-          adjustedValue = (newValue) % this.max
-          if (adjustedValue < 0) {
-            adjustedValue = 360 + adjustedValue 
+          if (this.selected) {
+            adjustedValue = Math.max(0, Math.min(newValue, 360))
+          } else {
+            adjustedValue = (newValue) % this.max
+            if (adjustedValue < 0) {
+              adjustedValue = 360 + adjustedValue 
+            }
           }
         } else {
           adjustedValue = Math.max(0, Math.min(newValue, 100))
